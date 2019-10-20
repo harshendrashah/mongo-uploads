18d431850360e2dcbd70bb07916a57a3d0ec39e4
harshendrashah <harshendrashah812@gmail.com>
2019-10-20 19:11:04 +0530

    db connection changes

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //Mongo URI
-const mongoURI = 'mongodb://127.0.0.1:27017/mongo-uploads';
+const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mongo-uploads';
 
 //Create mongo connection
 const conn = mongoose.createConnection(mongoURI);
