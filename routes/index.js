392bd4a728e3329faac9997d2711fb2414f32522
harshendrashah <harshendrashah812@gmail.com>
2019-10-20 19:39:38 +0530

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -4,48 +4,15 @@ const { upload, getGFS } = require('../config/configureStorage');
 const router = new express.Router()
 
 router.get('/', (req, res) => {
-    getGFS().files.find().toArray((err, files) => {
-        // Check if files
-        if (!files || files.length === 0) {
-        res.render('index', { files: false });
-        } else {
-        files.map(file => {
-            if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-                file.isImage = true;
-            } else {
-                file.isImage = false;
-            }
-        });
-        res.render('index', { files: files });
-        }
-    });
+    res.render('index');
 });
 
 // Use the input type name you used in the form instead of 'file'
 router.post('/upload', upload.single('file'), (req, res) => {
-    res.redirect('/')
-});
-
-router.get('/files', (req, res) => {
-    getGFS().files.find().toArray((err, files) => {
-        if(!files || files.length === 0) {
-            return res.status(404).json({
-                error: 'No files exist'
-            });
-        }
-        return res.json(files);
-    })
-});
-
-router.get('/files/:filename', (req, res) => {
-    getGFS().files.findOne({filename: req.params.filename}, (err, file) => {
-        if(!file) {
-            return res.status(404).json({
-                error: 'No file exists'
-            });
-        }
-        return res.json(file);
-    })
+    res.json({ 
+        message: "To view your file go to https://mongo-uploads-el.herokuapp.com/view/" + req.file.filename,
+        file: req.file
+    });
 });
 
 router.get('/view/:filename', (req, res) => {
